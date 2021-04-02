b82f833f187365f1b2af206f224f06d06e2d0340
Armel Soro <armel.soro@cosmotech.com>
2021-04-02 10:33:02 +0200

    Provide both sync and async Cosmos clients as common Spring beans
    They will be much more easier and integrate in the different services.
    
    The fact that they also implement Closeable makes them auto-destroyable
    with the application context.

--- common/src/main/kotlin/com/cosmotech/api/CsmApiConfiguration.kt
+++ common/src/main/kotlin/com/cosmotech/api/CsmApiConfiguration.kt
@@ -2,6 +2,12 @@
 // Licensed under the MIT license.
 package com.cosmotech.api
 
+import com.azure.cosmos.CosmosAsyncClient
+import com.azure.cosmos.CosmosClient
+import com.azure.cosmos.CosmosClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
@@ -9,6 +15,23 @@ import springfox.documentation.spring.web.DocumentationCache
 import springfox.documentation.spring.web.plugins.DocumentationPluginsManager
 import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider
 import springfox.documentation.swagger.web.SwaggerResource
+
+@Configuration
+class CsmApiConfiguration {
+
+  @Value("\${azure.cosmosdb.serviceEndpoint}")
+  private lateinit var azureCosmosServiceEndpoint: String
+
+  @Value("\${azure.cosmosdb.key}") private lateinit var azureCosmosKey: String
+
+  @Bean
+  fun cosmosClientBuilder(): CosmosClientBuilder =
+      CosmosClientBuilder().endpoint(azureCosmosServiceEndpoint).key(azureCosmosKey)
+
+  @Bean fun cosmosClient(): CosmosClient = cosmosClientBuilder().buildClient()
+
+  @Bean fun cosmosAsyncClient(): CosmosAsyncClient = cosmosClientBuilder().buildAsyncClient()
+}
 
 @Component
 @Primary
